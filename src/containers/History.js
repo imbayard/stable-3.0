9d42d589e54ed3b39db03b6acb0003ca6bd79443
Bayard Eton <beton@bu.edu>
2021-11-24 17:03:50 -0500

    some styling

--- src/containers/History.js
+++ src/containers/History.js
@@ -7,17 +7,25 @@ export default function History() {
     const {report, colorScheme} = useAppContext();
     const isReportNull = (report === "");
     console.log(report);
-    const size0 = getSize(report[0].avg);
-    const size1 = getSize(report[1].avg);
-    const size2 = getSize(report[2].avg);
-    const size3 = getSize(report[3].avg);
-    const size4 = getSize(report[4].avg);
+    const size0 = getSize(report[0]);
+    const size1 = getSize(report[1]);
+    const size2 = getSize(report[2]);
+    const size3 = getSize(report[3]);
+    const size4 = getSize(report[4]);
 
-    function getSize(average){
-        const val = average * 1;
-        const percent = val * 100;
-        const str = percent.toString();
-        return str + "%";
+    function getSize(cat){
+        const val = cat.avg * 100;
+        var ret = (Math.round(val * 100) / 100).toFixed(0);
+        const str = ret.toString();
+        if(ret < 40){
+            ret = 40;
+        }
+        const size = ret.toString();
+        const out = {
+            'sizing': (size + "%"),
+            'str': (str + "%")
+        }
+        return out;
     }
     
     function renderHistory(){
@@ -27,11 +35,11 @@ export default function History() {
                 <p className='history-tip'>Hover over / tap a bar to see your weekly averages.</p>
                 <div className='history-circles-container'>
                     <span className='history-circles'>
-                        <div className='circle'><div style={{width: size0, backgroundColor: colorScheme.warn}} className='circle-value grow'>{report[0].cat} Average: {size0}</div></div>
-                        <div className='circle'><div style={{width: size1, backgroundColor: colorScheme.fadedWarn}} className='circle-value grow'>{report[1].cat} Average: {size1}</div></div>
-                        <div className='circle'><div style={{width: size2, backgroundColor: colorScheme.darkest}} className='circle-value grow'>{report[2].cat} Average: {size2}</div></div>
-                        <div className='circle'><div style={{width: size3, backgroundColor: colorScheme.darker}} className='circle-value grow'>{report[3].cat} Average: {size3}</div></div>
-                        <div className='circle'><div style={{width: size4, backgroundColor: colorScheme.success}} className='circle-value grow'>{report[4].cat} Average: {size4}</div></div>
+                        <div className='circle'><div style={{width: size0.sizing, backgroundColor: colorScheme.warn}} className='circle-value grow'>{report[0].cat} Average: {size0.str}</div></div>
+                        <div className='circle'><div style={{width: size1.sizing, backgroundColor: colorScheme.fadedWarn}} className='circle-value grow'>{report[1].cat} Average: {size1.str}</div></div>
+                        <div className='circle'><div style={{width: size2.sizing, backgroundColor: colorScheme.darkest}} className='circle-value grow'>{report[2].cat} Average: {size2.str}</div></div>
+                        <div className='circle'><div style={{width: size3.sizing, backgroundColor: colorScheme.darker}} className='circle-value grow'>{report[3].cat} Average: {size3.str}</div></div>
+                        <div className='circle'><div style={{width: size4.sizing, backgroundColor: colorScheme.success}} className='circle-value grow'>{report[4].cat} Average: {size4.str}</div></div>
                     </span>
                 </div>
             </div>
